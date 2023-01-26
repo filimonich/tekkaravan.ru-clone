dfeab35fe4d30f57000f3f6a4fa5bef0aacf00c7
_tango <shurinskiy@mail.ru>
2023-01-26 21:39:19 +0600

    add small modal forms

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -4,8 +4,19 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 (() => {
 	addUnderlay('modal');
 	makeModalFrame({ 
-		el: '.header__login, .header__logout, .navi__link_login', 
-		scrollLock
+		el: '.header__login, .header__logout, .navi__link_login, .form__small a', 
+		scrollLock,
+		open: function(el) {
+			document.querySelectorAll('.form__field_pass button').forEach(button => {
+				button.addEventListener('click', () => {
+					const inp = button.previousElementSibling;
+		
+					inp.getAttribute('type') == 'password' ? inp.setAttribute('type', 'text') : inp.setAttribute('type', 'password');
+					button.classList.toggle('active');
+				});
+			});
+		
+		}
 	});
 
 })();
