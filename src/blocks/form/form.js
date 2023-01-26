10b6262b9f63053a84338f10b8606653a48797de
_tango <shurinskiy@mail.ru>
2023-01-26 19:42:14 +0600

    add register form

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -2,4 +2,13 @@ import { selectTweaker } from "../../js/libs/selectTweaker";
 
 (() => {
 	selectTweaker(document.querySelectorAll('.form__field_sel'));
+
+	document.querySelectorAll('.form__field_pass button').forEach(button => {
+		button.addEventListener('click', () => {
+			const inp = button.previousElementSibling;
+
+			inp.getAttribute('type') == 'password' ? inp.setAttribute('type', 'text') : inp.setAttribute('type', 'password');
+			button.classList.toggle('active');
+		});
+	});
 })();
